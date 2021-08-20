23252a87f06591fa3ad1ca3a2879fbb0ddfcbccf
aruna <aruna@LAPTOP-MHE3B0OC>
2021-08-20 16:05:01 +0530

    commit the latest package

---
+++ FirstDemoProject/src/test/java/base/BaseTest.java
@@ -0,0 +1,23 @@
+package base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+	@Test
+	public void test()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.quit();
+	}
+
+}
